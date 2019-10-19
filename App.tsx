4f3620eca5bb2ad3788ba5678a2ae6422f65f42e
Alireza Ghamkhar <mra.ghamkhar@gmail.com>
2019-10-19 16:04:49 +0330

    Add setup for redux

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import RootNavigation from './src/config/navigation';
+import store from './src/config/redux/store';
 
 const App = () => {
-  return <RootNavigation />;
+  return (
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
+  );
 };
 
 export default App;
